4efbd1f95aeaeeb7fac91c0b150311748c0eded0
Ivan <ivan.yeong@shortcut.io>
2024-09-24 15:57:18 +0800

    Remove example options

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,13 +1,10 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
-    const color = document.getElementById('color').value;
-    const likesColor = document.getElementById('like').checked;
-
     const includedPlatforms = document.getElementById('includedPlatforms').value.replaceAll(",","/");
     const excludedPlatforms = document.getElementById('excludedPlatforms').value.replaceAll(",","/");
     
     chrome.storage.sync.set(
-      { favoriteColor: color, likesColor: likesColor, includedPlatforms: includedPlatforms, excludedPlatforms: excludedPlatforms },
+      { includedPlatforms: includedPlatforms, excludedPlatforms: excludedPlatforms },
       () => {
         // Update status to let user know options were saved.
         const status = document.getElementById('status');
@@ -23,11 +20,8 @@ const saveOptions = () => {
   // stored in chrome.storage.
   const restoreOptions = () => {
     chrome.storage.sync.get(
-      { favoriteColor: 'red', likesColor: true, includedPlatforms: "*", excludedPlatforms: "" },
+      { includedPlatforms: "*", excludedPlatforms: "" },
       (items) => {
-        document.getElementById('color').value = items.favoriteColor;
-        document.getElementById('like').checked = items.likesColor;
-
         document.getElementById('includedPlatforms').value = items.includedPlatforms;
         document.getElementById('excludedPlatforms').value = items.excludedPlatforms;
       }
