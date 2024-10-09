50e1d992b479b449d3d2478df64cc139b66457a8
Ivanseven <sir.ivanyeong@gmail.com>
2024-10-10 01:20:44 +0800

    Fix settings for postTypes

--- scripts/settings.js
+++ scripts/settings.js
@@ -4,18 +4,17 @@ import { FilterModeOptions } from "./constants.js"
 const saveOptions = () => {
     const filterMode = document.querySelector('input[name="filterMode"]:checked').value;
     const excludedPlatforms = document.getElementById('excludedPlatforms').value.replace(/\s/g, '').replaceAll("/",",");
-    const selectedPostTypes = {
+    const includedPostTypes = {
       game: document.querySelector('input[name="postTypeGame"]').checked === true,
       DLC: document.querySelector('input[name="postTypeDLC"]').checked === true,
       membership: document.querySelector('input[name="postTypeMembership"]').checked === true,
       credits: document.querySelector('input[name="postTypeCredits"]').checked === true,
-      preRelease: document.querySelector('input[name="postTypePreRelease"]').checked === true,
+      prerelease: document.querySelector('input[name="postTypePreRelease"]').checked === true,
       others: document.querySelector('input[name="postTypeOthers"]').checked === true,
     }
 
-
     chrome.storage.sync.set(
-      { filterMode, excludedPlatforms, selectedPostTypes},
+      { filterMode, excludedPlatforms, includedPostTypes},
       () => {
         // Update status to let user know options were saved.
         const status = document.getElementById('status');
@@ -29,9 +28,48 @@ const saveOptions = () => {
   
   // Restores options using the preferences stored in chrome.storage.
   const restoreOptions = () => {
+    const defaultPostTypes ={
+      game: true,
+      DLC: true,
+      membership: true,
+      credits: true,
+      prerelease: true,
+      others: true
+    }
+
     chrome.storage.sync.get(
-      { filterMode: "include", excludedPlatforms: "" },
+      { filterMode: "include", excludedPlatforms: "", includedPostTypes: defaultPostTypes},
       (items) => {
+        Object.keys(items.includedPostTypes).forEach((key)=>{
+          switch (key) {
+            case "game":
+              document.getElementById('postTypeGame').checked = items.includedPostTypes[key]
+              break;
+
+            case "DLC":
+              document.getElementById('postTypeDLC').checked = items.includedPostTypes[key]
+              break;
+
+            case "membership":
+              document.getElementById('postTypeMembership').checked = items.includedPostTypes[key]
+              break;
+
+            case "credits":
+              document.getElementById('postTypeCredits').checked = items.includedPostTypes[key]
+              break; 
+
+            case "prerelease":
+              document.getElementById('postTypePreRelease').checked = items.includedPostTypes[key]
+              break; 
+
+            case "others":
+              document.getElementById('postTypeOthers').checked = items.includedPostTypes[key]
+              break; 
+              
+            default:
+              break;
+          }
+        })
 
         if (items.filterMode === FilterModeOptions.INCLUDE) {
             document.getElementById('filterInclude').checked = true
