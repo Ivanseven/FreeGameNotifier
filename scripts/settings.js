69125f1d0e0c137b515257c4d5dd4f921a1f8209
Ivanseven <sir.ivanyeong@gmail.com>
2024-10-10 00:20:53 +0800

    Add settings for postType

--- scripts/settings.js
+++ scripts/settings.js
@@ -4,9 +4,18 @@ import { FilterModeOptions } from "./constants.js"
 const saveOptions = () => {
     const filterMode = document.querySelector('input[name="filterMode"]:checked').value;
     const excludedPlatforms = document.getElementById('excludedPlatforms').value.replace(/\s/g, '').replaceAll("/",",");
-    
+    const selectedPostTypes = {
+      game: document.querySelector('input[name="postTypeGame"]').checked === true,
+      DLC: document.querySelector('input[name="postTypeDLC"]').checked === true,
+      membership: document.querySelector('input[name="postTypeMembership"]').checked === true,
+      credits: document.querySelector('input[name="postTypeCredits"]').checked === true,
+      preRelease: document.querySelector('input[name="postTypePreRelease"]').checked === true,
+      others: document.querySelector('input[name="postTypeOthers"]').checked === true,
+    }
+
+
     chrome.storage.sync.set(
-      { filterMode: filterMode, excludedPlatforms: excludedPlatforms },
+      { filterMode, excludedPlatforms, selectedPostTypes},
       () => {
         // Update status to let user know options were saved.
         const status = document.getElementById('status');
